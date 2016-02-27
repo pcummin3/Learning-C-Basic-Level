14eb773920ca406362687cddd4d7782500aaaee1
Paul Cummings <pcummin3@uncc.edu>
2016-02-26 22:33:28 -0500

    Added files via upload
    Lesson 1 instructions and a sample completed project for lesson 1.  Use my .cpp for comparison if you wish.

---
+++ Lesson 1/Lesson 1 Example/main.cpp
@@ -0,0 +1,20 @@
+//This is my first comment!
+
+/*
+  Author:  Paul Cummings
+  Program Name:  main.cpp
+  Version Date:  26 February 2016
+  Description:  This program tells the user:  Hello World!
+*/
+
+#include <iostream>  //Includes the iostream package for use.
+
+using namespace std;  //This includes the standard C++ library.
+
+int main()  //Our main() function.  All of our program is contained within.
+{
+    cout << "Hello world!" << endl;
+    //This cout statement prints "Hello World!" for the user.
+
+    return 0;  //This return statement terminates the program.
+}
